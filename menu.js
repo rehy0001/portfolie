39132bcf533a0edee69d6e2ecf5342921b8567ab
Rebekka <rehy0001@stud.kea.dk>
2024-01-09 23:36:17 +0100

    krediteret

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+/************* burgermenu - har kopieret dette fra undervisningen ***********************/
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
